3f88acda652e377ce071de63f7a9a351b40bdb1c
anfqlc1127@gmail.com <anfqlc1127@gmail.com>
2022-03-22 21:16:53 +0900

    IoU

---
+++ basic_object_detection/IoU/IoU.py
@@ -0,0 +1,60 @@
+"""
+Reference:
+    Aladdin Persson YOUTUBE: "https://www.youtube.com/watch?v=XXYG5ZWtjj0&list=PLhhyoLH6Ijfw0TpCTVTNk42NN08H6UvNq&index=2&ab_channel=AladdinPersson"
+"""
+
+import torch
+
+def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
+
+    """
+    Calculates intersection over union
+    Parameters:
+        boxes_preds (tensor): Predictions of Bounding Boxes (BATCH_SIZE, 4)
+        boxes_labels (tensor): Correct Labels of Boxes (BATCH_SIZE, 4)
+        box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
+    Returns:
+        tensor: Intersection over union for all examples
+    """
+
+    if box_format == "midpoint":
+        box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+        box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+        box1_x2 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+        box1_y2 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+        box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+        box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+        box2_x2 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+        box2_y2 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+
+    elif box_format == "corners":
+
+        # boxes_pred shape is (N, 4) where N is the number of bboxes
+        # boxes_labels shape is (N,4)
+        # [:] Slicing을 하는 이유: [:]을 하면 shape을 유지한채 반환한다. (N, 1)
+        # 반대로 indexing을 하면 (N,)이 반환된다.
+        box1_x1 = boxes_preds[..., 0:1]
+        box1_y1 = boxes_preds[..., 1:2]
+        box1_x2 = boxes_preds[..., 2:3]
+        box1_y2 = boxes_preds[..., 3:4]
+
+        box2_x1 = boxes_labels[..., 0:1]
+        box2_y1 = boxes_labels[..., 1:2]
+        box2_x2 = boxes_labels[..., 2:3]
+        box2_y2 = boxes_labels[..., 3:4]
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    # .clamp(0) is for the case when they do not intersect
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y1 - box1_y2))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y1 - box2_y2))
+
+    return intersection / (box1_area + box2_area - intersection)
+
+
+
